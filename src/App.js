340ccd0a5488730b4c920107c27fc98bffcfbce5
imsaradi <41167693+imsaradi@users.noreply.github.com>
2026-08-05 12:40:02 -0400

    Fix formatting of apiUrl declaration

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredTerms, setFilteredTerms] = useState([]);
 
-  const apiUrl = 'https://66veklrd65.execute-api.us-east-1.amazonaws.com/dev
-'; // Replace with your API Gateway URL
+  const apiUrl = 'https://66veklrd65.execute-api.us-east-1.amazonaws.com/dev'; // Replace with your API Gateway URL
 
   const handleSearch = () => {
     console.log('Fetching data from API...');
